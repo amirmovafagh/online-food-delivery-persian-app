54db40bb9e924d1065b975d561536ddf2d7627ae
amir <amir.movafagh@gmail.com>
2019-12-01 20:13:44 +0330

    remove navigation binding adapter , add navigate function in mainActivity

--- app/src/main/java/ir/boojanco/onlinefoodorder/ui/activities/MainActivity.java
+++ app/src/main/java/ir/boojanco/onlinefoodorder/ui/activities/MainActivity.java
@@ -2,16 +2,27 @@ package ir.boojanco.onlinefoodorder.ui.activities;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import ir.boojanco.onlinefoodorder.R;
 
 import ir.boojanco.onlinefoodorder.databinding.ActivityMainBinding;
 import ir.boojanco.onlinefoodorder.models.user.RegisterUserResponse;
 import ir.boojanco.onlinefoodorder.ui.base.BaseActivity;
+import ir.boojanco.onlinefoodorder.ui.fragments.CartFragment;
+import ir.boojanco.onlinefoodorder.ui.fragments.HomeFragment;
+import ir.boojanco.onlinefoodorder.ui.fragments.RestaurantFragment;
+import ir.boojanco.onlinefoodorder.ui.fragments.UserProfileFragment;
 import ir.boojanco.onlinefoodorder.ui.navigator.MainNavigator;
 import ir.boojanco.onlinefoodorder.viewmodels.MainViewModel;
 
@@ -19,9 +30,14 @@ import ir.boojanco.onlinefoodorder.viewmodels.MainViewModel;
 public class MainActivity extends BaseActivity implements MainNavigator {
     private static String TAG = "regTest";
     private MainViewModel mainViewModel;
-
     ActivityMainBinding binding;
-
+    final Fragment homeFragment = new HomeFragment();
+    final Fragment cartFragment = new CartFragment();
+    final Fragment restaurantFragment = new RestaurantFragment();
+    final Fragment userProfileFragment = new UserProfileFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = homeFragment;
+    BottomNavigationView bottomNavigationView ;
 
 
     @Override
@@ -36,7 +52,37 @@ public class MainActivity extends BaseActivity implements MainNavigator {
         // Specify the current activity as the lifecycle owner.
         binding.setLifecycleOwner(this);
 
+        fm.beginTransaction().add(R.id.fragment_frame_layout, cartFragment, "3").hide(cartFragment).commit();
+        fm.beginTransaction().add(R.id.fragment_frame_layout, userProfileFragment, "3").hide(userProfileFragment).commit();
+        fm.beginTransaction().add(R.id.fragment_frame_layout, restaurantFragment, "2").hide(restaurantFragment).commit();
+        fm.beginTransaction().add(R.id.fragment_frame_layout, homeFragment, "1").commit();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+                switch (item.getItemId()) {
+                    case R.id.home_fragment:
+                        fm.beginTransaction().hide(active).show(homeFragment).commit();
+                        active = homeFragment;
+                        return true;
 
+                    case R.id.cart_fragment:
+                        fm.beginTransaction().hide(active).show(cartFragment).commit();
+                        active = cartFragment;
+                        return true;
+
+                    case R.id.user_profile_fragment:
+                        fm.beginTransaction().hide(active).show(userProfileFragment).commit();
+                        active = userProfileFragment;
+                        return true;
+                    case R.id.restaurants_fragment:
+                        fm.beginTransaction().hide(active).show(restaurantFragment).commit();
+                        active = restaurantFragment;
+                        return true;
+                }
+                return false;
+            };
 }
--- app/src/main/java/ir/boojanco/onlinefoodorder/viewmodels/MainViewModel.java
+++ app/src/main/java/ir/boojanco/onlinefoodorder/viewmodels/MainViewModel.java
@@ -11,13 +11,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import ir.boojanco.onlinefoodorder.ui.base.BaseViewModel;
 import ir.boojanco.onlinefoodorder.ui.navigator.MainNavigator;
 
-public class MainViewModel extends BaseViewModel<MainNavigator> implements BottomNavigationView.OnNavigationItemSelectedListener{
+public class MainViewModel extends BaseViewModel<MainNavigator> {
     private static final String TAG =MainViewModel.class.getSimpleName();
 
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        Log.e(TAG, ""+menuItem.getItemId());
-        return false;
-    }
 }
